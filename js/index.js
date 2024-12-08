52b0346fc84f69b2944de7bb567652a99901245a
lunastudio-art <lunastudio403@gmail.com>
2024-12-07 22:58:56 -0500

    fix paths

--- js/index.js
+++ js/index.js
@@ -6,14 +6,14 @@ function placeHolderAlert(realPlace) {
 }
 
 function returnHeader() {
-  fetch("../src/header.html")
+  fetch("/aspireaccess/src/header.html")
     .then((response) => response.text())
     .then((data) => (document.getElementById("header").innerHTML = data))
     .catch((err) => console.error("Header loading failed:", err));
 }
 
 function returnFooter() {
-  fetch("../src/footer.html")
+  fetch("/aspireaccess/src/footer.html")
     .then((response) => response.text())
     .then((data) => (document.getElementById("footer").innerHTML = data))
     .catch((err) => console.error("Footer loading failed:", err));
