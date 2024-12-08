a6cceeb76826e66c8f78200a8a3e04b37ddf5fb5
lunastudio-art <lunastudio403@gmail.com>
2024-12-08 10:40:28 -0500

    QOL Fixes for chat

--- js/chatbot.js
+++ js/chatbot.js
@@ -22,11 +22,23 @@ function getBotResponse(input) {
         "hi": "Hello, I'm AspireGPT! Before we get started, can I have your name?",
         "luna": "Hi there! What course do you want to take?",
         "ap computer science a": "Okay. Can I have your zip code so I can see if this course is available in your area?",
-        "19130": "Okay. I have just confirmed that this course is not available in your zip code. We will be getting in touch with your local library soon. In the meantime, can I have your email so that I can notify you when this course becomes available to you?",
+        "00001": "Okay. I have found that the cost of this course could be lowered with help from the public library system. We will be getting in touch with your local library soon. In the meantime, can I have your email so that I can notify you when this course becomes available to you?",
         "lunastudio403@gmail.com": "Got it. Thank you for helping us fulfil our mission! We will be in touch with you soon."
     };
+    
     var response = botResponses[input.toLowerCase()] || "Sorry, I didn't understand that.";
+    var sendButton = document.getElementById("sendButton");
+    var userInput = document.getElementById("userInput");
     addMessage(response, "bot-message");
+    if (response == "Got it. Thank you for helping us fulfil our mission! We will be in touch with you soon.")
+    {
+        sendButton.hidden = true;
+        userInput.hidden = true;
+        
+        addMessage("AspireGPT has closed this chat.");
+
+    }
+    
 }
 
 document.getElementById("userInput").addEventListener("keydown", function(event) { 
